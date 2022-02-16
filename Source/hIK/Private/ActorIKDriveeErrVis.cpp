2a7abd6269dca12de5c16763aea564e9398878ee
wanxwang <wanxin-wang@uiowa.edu>
2022-02-16 14:49:35 -0600

    high-light selected bone

--- Source/hIK/Private/ActorIKDriveeErrVis.cpp
+++ Source/hIK/Private/ActorIKDriveeErrVis.cpp
@@ -64,7 +64,6 @@ void AActorIKDriveeErrVis::UpdateBoneVis(int32 boneID_k)
 	}
 
 	LOGIKVar(LogInfoInt, meshComp->GetNumBones());
-	// failed to verify the functions: FSkinWeightVertexBuffer::GetBoneIndex, FSkinWeightVertexBuffer::GetBoneWeight
 	FSkinWeightVertexBuffer* buffer = nullptr;
 	const FSkeletalMeshLODRenderData* renderData = nullptr;
 	int32 lod = 0;
@@ -110,14 +109,6 @@ void AActorIKDriveeErrVis::UpdateBoneVis(int32 boneID_k)
 				for (uint32 i_influence = 0; i_influence < buffer->GetMaxBoneInfluences(); i_influence++)
 				{
 					int32 id_g = buffer->GetBoneIndex(i_v, i_influence);
-					// const int8 threshold = (int8)(0.1f*256.0f);
-					if (0 != id_g)
-					{
-					// FName name_i = meshComp->GetBoneName(id_g);
-					// LOGIKVar(LogInfoWCharPtr, *name_i.ToString());
-					// LOGIKVar(LogInfoInt, buffer->GetBoneIndex(i_v, i_influence));
-					// LOGIKVar(LogInfoInt, buffer->GetBoneWeight(i_v, i_influence));
-					}
 					if (boneID_g == id_g)
 					{
 						uint8 weight_i = buffer->GetBoneWeight(i_v, i_influence);
@@ -136,49 +127,6 @@ void AActorIKDriveeErrVis::UpdateBoneVis(int32 boneID_k)
 		LOGIKVar(LogInfoWCharPtr, *name_i.ToString());
 		meshComp->SetVertexColorOverride(lod-1, clrVert);
 	}
-
-	// LOGIKVar(LogInfoPtr, buffer);
-	// LOGIKVar(LogInfoInt, lod);
-	// LOGIKVar(LogInfoInt, buffer->GetNumVertices());
-
-
-	// // proved wrong:
-
-	// int n_bones = meshComp->GetNumBones();
-	// // TArray<int32> boneIdx; boneIdx.SetNum(n_bones);
-	// TArray<int32> vcount;
-	// vcount.Init(0, n_bones);
-
-	// for (uint32 i_v = 0; i_v < buffer->GetNumVertices(); i_v++)
-	// {
-	//  	for (int i_w = 0; i_w < 4; i_w++) //MAX_TOTAL_INFLUENCES
-	//  	{
-	//  		int i_bone = buffer->GetBoneIndex(i_v, i_w);
-	//  		if (0 != i_bone)
-	//  		{
-	//  			// FName name_i = meshComp->GetBoneName(i_bone);
-	//  			// LOGIKVar(LogInfoWCharPtr, *name_i.ToString());
-	//  			// LOGIKVar(LogInfoInt, buffer->GetBoneIndex(i_v, i_w));
-	//  			// LOGIKVar(LogInfoInt, buffer->GetBoneWeight(i_v, i_w));
-	//  		}
-	// 		vcount[i_bone] ++;
-	//  	}
-	// }
-
-	// for (int i_bone = 0; i_bone < n_bones; i_bone++)
-	// {
-	// 	FName name_i = meshComp->GetBoneName(i_bone);
-	// 	LOGIKVar(LogInfoWCharPtr, *name_i.ToString());
-	// 	LOGIKVar(LogInfoInt, vcount[i_bone]);
-	// }
-
-	// // end of proof
-
-
-
-
-
-
 	// const FSkinWeightDataVertexBuffer* bufferData = buffer->GetDataVertexBuffer();
 	// LOGIKVar(LogInfoInt, bufferData->GetNumVertices());
 	// LOGIKVar(LogInfoInt, bufferData->GetNumBones());
